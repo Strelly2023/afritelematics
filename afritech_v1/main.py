f8513e8ed8709ff198b025a7a26136a831d5c59d
Djuma Kikombe <djuma.kikombe@yahoo.com>
2026-05-02 23:16:57 +1000

    freeze v1 execution: authority transferred to afritech

--- afritech_v1/main.py
+++ afritech_v1/main.py
@@ -1,4 +1,10 @@
 from afritech_v1.runtime.boot import run_boot_contract
 
+
+raise SystemExit(
+    "AfriTech v1 is constitutionally frozen. "
+    "Use `python3 -m afritech.main`."
+)
+
 if __name__ == "__main__":
     run_boot_contract()
